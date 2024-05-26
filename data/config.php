229ad06e8d94b80aaed5990f5998db1046ba3632
opensh <125738740+openMJJ@users.noreply.github.com>
2024-05-26 19:25:13 +0800

    Add files via upload

---
+++ data/config.php
@@ -0,0 +1,11 @@
+<?php
+//数据库配置
+$db_config = array(
+    "type" => "mysql", //类型
+	"host" => "localhost", //地址
+	"port" => 3306, //端口
+	"name" => "twonav", //库名
+	"user" => "twonav", //账号
+	"password" => "dEbeXyaPeL2p2G2A" //密码
+);
+?>
